a0aec5cfc2f9ed62ed570a6ea306b86773642f59
Md Abubakar <56873234+AbubakarMd@users.noreply.github.com>
2023-01-22 00:16:10 +0530

    Create InsertSort.js

---
+++ InsertSort.js
@@ -0,0 +1,17 @@
+
+
+
+function sorting(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let value = arr[i];
+        let sort = i - 1;
+        while (sort >= 0 && arr[sort] > value) {
+            arr[sort + 1] = arr[sort];
+            sort = sort - 1;
+        }
+        arr[sort + 1] = value;
+    }
+}
+arr = [3, 7, 1, 2, 0]
+sorting(arr)
+console.log(arr);
